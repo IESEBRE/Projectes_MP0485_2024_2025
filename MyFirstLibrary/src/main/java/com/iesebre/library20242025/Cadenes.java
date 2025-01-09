762131f0471b1ebc6da4a16280558baf036ed34b
Santi Sabate <ssabate@iesebre.com>
2025-01-09 09:48:56 +0100

    Actualitzat a 20-12-2024

---
+++ MyFirstLibrary/src/main/java/com/iesebre/library20242025/Cadenes.java
@@ -0,0 +1,4 @@
+package com.iesebre.library20242025;
+
+public class Cadena {
+}
---
+++ UsingMyFirstLibrary/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import static com.iesebre.library20242025.Cadenes.retornaCaracter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(retornaCaracter());
+    }
+}

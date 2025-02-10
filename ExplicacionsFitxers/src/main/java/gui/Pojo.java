19a135152fb30ce28947d0a3bc4ffe24424a20e7
Santi Sabate <ssabate@iesebre.com>
2025-02-10 14:16:54 +0100

    Actualitzat a 10/2/2025. CRUD fer sense fitxers

---
+++ ExplicacionsFitxers/src/main/java/gui/Pojo.java
@@ -0,0 +1,42 @@
+package gui;
+
+import java.io.Serializable;
+
+public class Pojo implements Serializable {
+
+    private String nom;
+    private String cognoms;
+
+    public Pojo() {
+    }
+
+    public Pojo(String nom, String cognoms) {
+        this.nom = nom;
+        this.cognoms = cognoms;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognoms() {
+        int i=0;
+        return cognoms;
+    }
+
+    public void setCognoms(String cognoms) {
+        this.cognoms = cognoms;
+    }
+
+    @Override
+    public String toString() {
+        return "Pojo{" +
+                "nom='" + nom + '\'' +
+                ", cognoms='" + cognoms + '\'' +
+                '}';
+    }
+}

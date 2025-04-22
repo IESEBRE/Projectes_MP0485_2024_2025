a2ec32b20e56a434bcc1cd42519e93a18a4058a1
Santi Sabate <ssabate@iesebre.com>
2025-04-22 09:40:40 +0200

    Exemple d'ús d'expressió regular. Actualitzat a 22/4/2025

--- ExplicacionsUF5/src/main/java/org/example/gui/mvc/controller/Controller.java
+++ ExplicacionsUF5/src/main/java/org/example/gui/mvc/controller/Controller.java
@@ -4,8 +4,11 @@ import org.example.gui.mvc.model.Model;
 import org.example.gui.mvc.view.View;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class Controller {
     
@@ -23,12 +26,42 @@ public class Controller {
     //Lògica de l'aplicació
     private void controla() {
 
+        //Propietats de la vista
+        vista.setPreferredSize(new Dimension(600,600));
+        vista.setContentPane(vista.getPanel());
+        vista.setLocationByPlatform(true);
+        vista.pack();
+        vista.setVisible(true);
+
         //Anem a posar el codi al clic del ratolí
         vista.getButton1().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 model.setNumClics(model.getNumClics()+1);
                 JOptionPane.showMessageDialog(vista.getButton1(),"M'has apretat "+model.getNumClics()+" vegades!!");
+            }
+        });
+
+        //Codi de la pèrdua de foco del camp de text
+        vista.getTextField1().addFocusListener(new FocusAdapter() {
+            /**
+             * Invoked when a component loses the keyboard focus.
+             *
+             * @param e
+             */
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                //Text escrit al component JTextField
+                String text=vista.getTextField1().getText();
+                //Mirem si el text complix un patró (comença per lletra majúscyula i està seguit d'1 o més majúscules)
+                if(!text.matches("\\p{Upper}[\\p{Lower}]+")) {
+                    //Seleccionem el text del JTextField, així és més fàcil borrar-lo i tornar a escriure
+                    vista.getTextField1().setSelectionStart(0);
+                    vista.getTextField1().setSelectionEnd(text.length());
+                    //Impedim la pèrdua de foco del component
+                    vista.getTextField1().requestFocusInWindow();
+                };
             }
         });
 
--- ExplicacionsUF5/src/main/java/org/example/gui/mvc/view/View.java
+++ ExplicacionsUF5/src/main/java/org/example/gui/mvc/view/View.java
@@ -1,20 +1,30 @@
 package org.example.gui.mvc.view;
 
 import javax.swing.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class View extends JFrame {
     private JPanel panel;
     private JButton button1;
+    private JTextField textField1;
 
     public View(){
-        setContentPane(panel);
-        pack();
-        setVisible(true);
+
     }
 
     //getters pel controlador
 
+
+    public JPanel getPanel() {
+        return panel;
+    }
+
     public JButton getButton1() {
         return button1;
+    }
+
+    public JTextField getTextField1() {
+        return textField1;
     }
 }
